3c53b4bfb916c06661654330d54b08bd11995212
CHEN Xian'an <xianan.chen@gmail.com>
2013-01-03 00:11:41 +0800

    Initial Commit

---
+++ CXAMenuItem/CXAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CXAAppDelegate.h
+//  CXAMenuItem
+//
+//  Created by Chen Xian'an on 1/3/13.
+//  Copyright (c) 2013 lazyapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
